a950198e17cca45ac659f296f30b745c3272d45a
Yahaha <634597805@qq.com>
2019-11-21 09:48:22 +0800

    添加获取摇杆档位方法

--- app/src/main/java/com/example/roadroller/RockerView.java
+++ app/src/main/java/com/example/roadroller/RockerView.java
@@ -14,6 +14,46 @@ import android.view.SurfaceView;
 
 public class RockerView extends SurfaceView implements Callback, Runnable {
 
+	//速度档位
+	public int nowSpeedGear;
+	public int speedGear = SPEED_GEAR_0;
+	public static final int SPEED_GEAR_8 = 8;
+	public static final int SPEED_GEAR_7 = 7;
+	public static final int SPEED_GEAR_6 = 6;
+	public static final int SPEED_GEAR_5 = 5;
+	public static final int SPEED_GEAR_4 = 4;
+	public static final int SPEED_GEAR_3 = 3;
+	public static final int SPEED_GEAR_2 = 2;
+	public static final int SPEED_GEAR_1 = 1;
+	public static final int SPEED_GEAR_0 = 0;
+
+	//方向档位
+	public int nowDirectionGear;
+	public int directionGear = DIRECTION_GEAR_0;
+	public static final int DIRECTION_GEAR_20 = 20;
+	public static final int DIRECTION_GEAR_19 = 19;
+	public static final int DIRECTION_GEAR_18 = 18;
+	public static final int DIRECTION_GEAR_17 = 17;
+	public static final int DIRECTION_GEAR_16 = 16;
+	public static final int DIRECTION_GEAR_15 = 15;
+	public static final int DIRECTION_GEAR_14 = 14;
+	public static final int DIRECTION_GEAR_13 = 13;
+	public static final int DIRECTION_GEAR_12 = 12;
+	public static final int DIRECTION_GEAR_11 = 11;
+	public static final int DIRECTION_GEAR_10 = 10;
+	public static final int DIRECTION_GEAR_9 = 9;
+	public static final int DIRECTION_GEAR_8 = 8;
+	public static final int DIRECTION_GEAR_7 = 7;
+	public static final int DIRECTION_GEAR_6 = 6;
+	public static final int DIRECTION_GEAR_5 = 5;
+	public static final int DIRECTION_GEAR_4 = 4;
+	public static final int DIRECTION_GEAR_3 = 3;
+	public static final int DIRECTION_GEAR_2 = 2;
+	public static final int DIRECTION_GEAR_1 = 1;
+	public static final int DIRECTION_GEAR_0 = 0;
+
+
+
 	String TAG = "MainActivity";
 	/**thread*/
 	private Thread th;
@@ -137,15 +177,21 @@ public class RockerView extends SurfaceView implements Callback, Runnable {
 //			canvas.drawARGB(0,255,255,255);
 			//绘制左摇杆背景
 			paint.setColor(0x70ffffff);
-			canvas.drawRect(RockerRectX1 - SmallRockerCircleR1,RockerRectY1 - 250,
-					RockerRectX1 + SmallRockerCircleR1,RockerRectY1 + 250,paint);
+			canvas.drawRect(RockerRectX1 - SmallRockerCircleR1,
+							RockerRectY1 - 250,
+							RockerRectX1 + SmallRockerCircleR1,
+							RockerRectY1 + 250,
+							paint);
 			//绘制左摇杆
 			paint.setColor(0xaaffffff);
 			canvas.drawCircle(SmallRockerCircleX1, SmallRockerCircleY1, SmallRockerCircleR1, paint);
 			//绘制右摇杆背景
             paint.setColor(0x70ffffff);
-			canvas.drawRect(RockerRectX2 - 250,RockerRectY2 - SmallRockerCircleR2,
-					RockerRectX2 + 250,RockerRectY2 + SmallRockerCircleR2,paint);
+			canvas.drawRect(RockerRectX2 - 250,
+							RockerRectY2 - SmallRockerCircleR2,
+							RockerRectX2 + 250,
+							RockerRectY2 + SmallRockerCircleR2,
+							paint);
 			//绘制右摇杆
             paint.setColor(0xaaffffff);
 			canvas.drawCircle(SmallRockerCircleX2, SmallRockerCircleY2, SmallRockerCircleR2, paint);
@@ -183,12 +229,14 @@ public class RockerView extends SurfaceView implements Callback, Runnable {
 			if(leftOrRight == 1){
 				if ((int) event.getY() >=  (RockerRectY1-250) && (int) event.getY() <=  (RockerRectY1+250)){
 					SmallRockerCircleY1 = (int) event.getY();
+					nowSpeedGear = getSpeedGear((int) event.getY());
 					}
 				flagIn = true;
 				return true;
 			}
 			if(leftOrRight == 2){
 				SmallRockerCircleX2 = (int) event.getX();
+				nowDirectionGear = getDirectionGear((int)SmallRockerCircleX2);
 				flagIn2 = true;
 				return true;
 			}
@@ -220,6 +268,7 @@ public class RockerView extends SurfaceView implements Callback, Runnable {
                             && (int) event.getY() >=  RockerRectY1-250
                             && (int) event.getY() <=  RockerRectY1+250){
                         SmallRockerCircleY1 = (int) event.getY();
+						nowSpeedGear = getSpeedGear((int) event.getY());
                     }
 				}
 				//右边
@@ -230,6 +279,7 @@ public class RockerView extends SurfaceView implements Callback, Runnable {
                             && (int) event.getY() >=  RockerRectY2 - SmallRockerCircleR2
                             && (int) event.getY() <=  RockerRectY2 + SmallRockerCircleR2){
                         SmallRockerCircleX2 = (int) event.getX();
+						nowDirectionGear = getDirectionGear((int)SmallRockerCircleX2);
                     }
 				}
 			}
@@ -244,6 +294,7 @@ public class RockerView extends SurfaceView implements Callback, Runnable {
                                 && (int) event.getY() >=  RockerRectY1-250
                                 && (int) event.getY() <=  RockerRectY1+250){
                             SmallRockerCircleY1 = (int) event.getY();
+							nowSpeedGear = getSpeedGear((int) event.getY());
                         }
 						/**************************处理第二个按下的*****************************/
 						// 当触屏区域在活动范围内
@@ -252,6 +303,7 @@ public class RockerView extends SurfaceView implements Callback, Runnable {
                                 && (int) event.getY(1) >=  RockerRectY2 - SmallRockerCircleR2
                                 && (int) event.getY(1) <=  RockerRectY2 + SmallRockerCircleR2){
                             SmallRockerCircleX2 = (int) event.getX(1);
+							nowDirectionGear = getDirectionGear((int)SmallRockerCircleX2);
                         }
 					}
 					if(leftOrRight == 2){
@@ -262,6 +314,7 @@ public class RockerView extends SurfaceView implements Callback, Runnable {
                                 && (int) event.getY() >=  RockerRectY2 - SmallRockerCircleR2
                                 && (int) event.getY() <=  RockerRectY2 + SmallRockerCircleR2){
                             SmallRockerCircleX2 = (int) event.getX();
+							nowDirectionGear = getDirectionGear((int)SmallRockerCircleX2);
                         }
 						/**************************处理第二个按下的*****************************/
 						// 当触屏区域在活动范围内
@@ -270,6 +323,7 @@ public class RockerView extends SurfaceView implements Callback, Runnable {
                                 && (int) event.getY(1) >=  RockerRectY1-250
                                 && (int) event.getY(1) <=  RockerRectY1+250){
                             SmallRockerCircleY1 = (int) event.getY(1);
+							nowSpeedGear = getSpeedGear((int) SmallRockerCircleY1);
                         }
 					}
 				}
@@ -282,6 +336,7 @@ public class RockerView extends SurfaceView implements Callback, Runnable {
                                 && (int) event.getY() >=  RockerRectY1-250
                                 && (int) event.getY() <=  RockerRectY1+250){
                             SmallRockerCircleY1 = (int) event.getY();
+							nowSpeedGear = getSpeedGear((int) event.getY());
                         }
                         //期间按下了右摇杆
                         if ((int) event.getX(1) >= RockerRectX2 - 250
@@ -289,6 +344,7 @@ public class RockerView extends SurfaceView implements Callback, Runnable {
                                 && (int) event.getY(1) >=  RockerRectY2 - SmallRockerCircleR2
                                 && (int) event.getY(1) <=  RockerRectY2 + SmallRockerCircleR2){
                             SmallRockerCircleX2 = (int) event.getX(1);
+							nowDirectionGear = getDirectionGear((int)SmallRockerCircleX2);
                             flagIn2 = true;
                         }
 					}
@@ -299,6 +355,7 @@ public class RockerView extends SurfaceView implements Callback, Runnable {
                                 && (int) event.getY(1) >=  RockerRectY1-250
                                 && (int) event.getY(1) <=  RockerRectY1+250){
                             SmallRockerCircleY1 = (int) event.getY(1);
+							nowSpeedGear = getSpeedGear((int) SmallRockerCircleY1);
                         }
 						// 当触屏区域在活动范围内
                         if ((int) event.getX() >= RockerRectX2 - 250
@@ -318,6 +375,7 @@ public class RockerView extends SurfaceView implements Callback, Runnable {
                                 && (int) event.getY(1) >=  RockerRectY2 - SmallRockerCircleR2
                                 && (int) event.getY(1) <=  RockerRectY2 + SmallRockerCircleR2){
                             SmallRockerCircleX2 = (int) event.getX(1);
+							nowDirectionGear = getDirectionGear((int)SmallRockerCircleX2);
                         }
 						// 当触屏区域在活动范围内
                         if ((int) event.getX() >= RockerRectX1-SmallRockerCircleR1
@@ -325,6 +383,7 @@ public class RockerView extends SurfaceView implements Callback, Runnable {
                                 && (int) event.getY() >=  RockerRectY1-250
                                 && (int) event.getY() <=  RockerRectY1+250){
                             SmallRockerCircleY1 = (int) event.getY();
+							nowSpeedGear = getSpeedGear((int) SmallRockerCircleY1);
                             flagIn = true;
                         }
 					}
@@ -335,6 +394,7 @@ public class RockerView extends SurfaceView implements Callback, Runnable {
                                 && (int) event.getY() >=  RockerRectY1-250
                                 && (int) event.getY() <=  RockerRectY1+250){
                             SmallRockerCircleY1 = (int) event.getY();
+							nowSpeedGear = getSpeedGear((int) SmallRockerCircleY1);
                         }
 						// 当触屏区域不在活动范围内
                         if ((int) event.getX(1) >= RockerRectX1-SmallRockerCircleR1
@@ -342,6 +402,7 @@ public class RockerView extends SurfaceView implements Callback, Runnable {
                                 && (int) event.getY(1) >=  RockerRectY1-250
                                 && (int) event.getY(1) <=  RockerRectY1+250){
                             SmallRockerCircleY1 = (int) event.getY(1);
+							nowSpeedGear = getSpeedGear((int) SmallRockerCircleY1);
                             flagIn = true;
                         }
 					}
@@ -386,6 +447,132 @@ public class RockerView extends SurfaceView implements Callback, Runnable {
 		CIRCLE_X2 = RockerRectX2;
 		CIRCLE_Y2 = RockerRectY2;
 		CIRCLE_SMALL_R2 = SmallRockerCircleR2;
+	}
+
+	public int getSpeedGear(int y){
+		int gear = 0;
+		int dif = Math.abs(y - RockerRectY1);
+		if (dif == 0){
+			gear = SPEED_GEAR_0;
+		}else {
+			switch (dif / 30) {
+				case 0:
+					gear = SPEED_GEAR_1;
+					break;
+				case 1:
+					gear = SPEED_GEAR_2;
+					break;
+				case 2:
+					gear = SPEED_GEAR_3;
+					break;
+				case 3:
+					gear = SPEED_GEAR_4;
+					break;
+				case 4:
+					gear = SPEED_GEAR_5;
+					break;
+				case 5:
+					gear = SPEED_GEAR_6;
+					break;
+				case 6:
+					gear = SPEED_GEAR_7;
+					break;
+				case 7:
+				case 8:
+					gear = SPEED_GEAR_8;
+					break;
+				default:
+					break;
+			}
+		}
+		if ((y - RockerRectY1) <= 0){
+			return gear;
+		}else{
+			return -gear;
+		}
+	}
+
+	/*
+	*获取左右方向档位
+	* 左右各20个档位
+	 */
+	public int getDirectionGear(int x){
+		int gear = 0;
+		int dif = Math.abs(x - RockerRectX2);
+		if (dif == 0){
+			gear = DIRECTION_GEAR_0;
+		}else {
+			switch (dif / 12) {
+				case 0:
+					gear = DIRECTION_GEAR_1;
+					break;
+				case 1:
+					gear = DIRECTION_GEAR_2;
+					break;
+				case 2:
+					gear = DIRECTION_GEAR_3;
+					break;
+				case 3:
+					gear = DIRECTION_GEAR_4;
+					break;
+				case 4:
+					gear = DIRECTION_GEAR_5;
+					break;
+				case 5:
+					gear = DIRECTION_GEAR_6;
+					break;
+				case 6:
+					gear = DIRECTION_GEAR_7;
+					break;
+				case 7:
+					gear = DIRECTION_GEAR_8;
+					break;
+				case 8:
+					gear = DIRECTION_GEAR_9;
+					break;
+				case 9:
+					gear = DIRECTION_GEAR_10;
+					break;
+				case 10:
+					gear = DIRECTION_GEAR_11;
+					break;
+				case 11:
+					gear = DIRECTION_GEAR_12;
+					break;
+				case 12:
+					gear = DIRECTION_GEAR_13;
+					break;
+				case 13:
+					gear = DIRECTION_GEAR_14;
+					break;
+				case 14:
+					gear = DIRECTION_GEAR_15;
+					break;
+				case 15:
+					gear = DIRECTION_GEAR_16;
+					break;
+				case 16:
+					gear = DIRECTION_GEAR_17;
+					break;
+				case 17:
+					gear = DIRECTION_GEAR_18;
+					break;
+				case 18:
+					gear = DIRECTION_GEAR_19;
+					break;
+				case 19:
+				case 20:
+					gear = DIRECTION_GEAR_20;
+					break;
+				default:
+					break;
+			}
+		}
+		if ((x - RockerRectX2) <= 0){
+			return gear;
+		}else{
+			return -gear;
+		}
 	}
 
 }
