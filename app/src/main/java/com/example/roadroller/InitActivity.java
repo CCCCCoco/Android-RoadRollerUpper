40bb136d188019bc0a87834961b073c677a88e45
Yahaha <634597805@qq.com>
2019-11-27 01:28:15 +0800

    使用handler代替timer

--- app/src/main/java/com/example/roadroller/InitActivity.java
+++ app/src/main/java/com/example/roadroller/InitActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -14,8 +15,17 @@ public class InitActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_init);
-        Timer timer = new Timer();
-        timer.schedule(new TimerTask() {
+//        Timer timer = new Timer();
+//        timer.schedule(new TimerTask() {
+//            @Override
+//            public void run() {
+//                Intent intent = new Intent(InitActivity.this, MainActivity.class);
+//                startActivity(intent);
+//                finish();
+//            }
+//        },2000);
+
+        new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 Intent intent = new Intent(InitActivity.this, MainActivity.class);
